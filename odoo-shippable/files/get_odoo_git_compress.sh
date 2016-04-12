a7ea50a67a4dbf00e7bf39e07aa379d2d78835e3
Moisés López <moylop260@vauxoo.com>
2016-04-12 17:05:00 -0500

    [REF] odoo-shippable: Small fix to compress in get_odoo_git_compress script without relative path

--- odoo-shippable/files/get_odoo_git_compress.sh
+++ odoo-shippable/files/get_odoo_git_compress.sh
@@ -6,10 +6,10 @@ git init --bare ${HOME}/odoo_git_compress/.git \
  ; git --git-dir=${HOME}/odoo_git_compress/.git remote add vauxoo https://github.com/vauxoo/odoo.git \
  ; git --git-dir=${HOME}/odoo_git_compress/.git fetch --all \
  && du -sh ${HOME}/odoo_git_compress/.git \
- && unbuffer git --git-dir=${HOME}/odoo_git_compress/.git gc --aggressive \
+ && git --git-dir=${HOME}/odoo_git_compress/.git gc --aggressive \
  && du -sh ${HOME}/odoo_git_compress/.git \
  && rm -f ${HOME}/odoo_git.tar.gz \
- && tar -zcvf ${HOME}/odoo_git.tar.gz ${HOME}/odoo_git_compress/.git \
+ && tar -zcvf ${HOME}/odoo_git.tar.gz -C ${HOME}/odoo_git_compress .git \
  && echo "Now you can upload the file ${HOME}/odoo_git.tar.gz"
 
  # && mkdir -p ${HOME}/addons-vauxoo/ \
